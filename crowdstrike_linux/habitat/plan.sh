a76d10ec1cede0c6b397f86946d3cf10cfda1d89
Dan Flores <danf425@gmail.com>
2020-02-05 12:28:32 -0500

    linux hab plan updated install hook

--- crowdstrike_linux/habitat/plan.sh
+++ crowdstrike_linux/habitat/plan.sh
@@ -4,9 +4,9 @@ pkg_version="5.24.10609"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_shasum="TODO"
-cid_shasum="4B0C4710019843FAAC999A96F6ECA3B8-CA"
+cid_shasum="E3264501FB484452B49CBA0F1BFBF595-1F"
 pkg_deps=(core/glibc core/bash)
-pkg_description="Crowdstrike agent "
+pkg_description="Crowdstrike agent for RHEL"
 
 do_install() {
   cp "" "$pkg_prefix/crowdstrike_linux_setup.rpm"
