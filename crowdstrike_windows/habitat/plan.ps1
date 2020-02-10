8df034eb5f0f042a0e89e5059ffb1af2921a72e3
Clark-Berroth <kris.clark-berroth@aig.com>
2020-02-10 16:55:29 -0600

    artificator inclusion

--- crowdstrike_windows/habitat/plan.ps1
+++ crowdstrike_windows/habitat/plan.ps1
@@ -7,8 +7,7 @@ $pkg_source="http://artifactory-am2.devops.aig.net/artifactory/chef-local/crowds
 $pkg_shasum="a141248820f9691906af86a21b7bcb42c35d07dfba31fbe1895aa2f349243254"
 $cid_shasum="E3264501FB484452B49CBA0F1BFBF595-1F"
 $windows_installer="WindowsSensor.exe"
-
-
+function Invoke-Unpack {}
 function Invoke-Install {
  Copy-Item -Path "$HAB_CACHE_SRC_PATH\$windows_installer" -Destination "$pkg_prefix\crowdstrike_windows_setup.exe"
 }
